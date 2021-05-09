3f578775d9f3141227c6f6d468cc20709d051f87
Wiktor <byrka.w@gmail.com>
2021-05-09 22:21:30 +0200

    Trying to Deploy

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 const { campgroundSchema, reviewSchema } = require("./schemas");
 const ExpressError = require("./utils/ExpressError");
-const Campground = require("./models/Campground");
-const Review = require("./models/Review");
+const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
